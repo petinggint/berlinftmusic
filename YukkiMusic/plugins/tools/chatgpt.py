236feab5b8c88d14b590d7cd5b004045067129e1
sɪ ᴧꝛᴧʙ <127276984+fadhilarab47@users.noreply.github.com>
2024-06-04 04:10:23 +0700

    Update chatgpt.py

--- YukkiMusic/plugins/tools/chatgpt.py
+++ YukkiMusic/plugins/tools/chatgpt.py
@@ -1,40 +1,41 @@
 import requests
-from pyrogram import filters
+from pyrogram import Client, filters
 from pyrogram.enums import ChatAction
 from TheApi import api
 
-from YukkiMusic import app
-
 
 @app.on_message(
-    filters.command(
-        ["chatgpt", "ai", "ask"], prefixes=["+", ".", "/", "-", "?", "$", "#", "&"]
-    )
+    filters.command(["chatgpt", "ai", "ask"], prefixes=["+", ".", "/", "-", "?", "$", "#", "&"])
 )
 async def chatgpt_chat(bot, message):
     if len(message.command) < 2 and not message.reply_to_message:
         await message.reply_text(
-            "Example:\n\n`/ai write simple website code using html css, js?`"
+            "Example:\n\n`/ai write simple website code using html, css, js?`"
         )
         return
 
+    # Determine the user input
     if message.reply_to_message and message.reply_to_message.text:
         user_input = message.reply_to_message.text
     else:
         user_input = " ".join(message.command[1:])
 
     try:
+        # Indicate that the bot is typing
         await bot.send_chat_action(message.chat.id, ChatAction.TYPING)
+        # Send the request to the API
         results = api.chatgpt(user_input)
+        # Check if the API call was successful and respond with the result
         if results["success"]:
             await message.reply_text(results["results"])
     except requests.exceptions.RequestException as e:
-        pass
+        await message.reply_text("There was an error processing your request. Please try again later.")
 
 
-__MODULE__ = "CʜᴀᴛGᴘᴛ"
+__MODULE__ = "ChatGPT"
 __HELP__ = """
-/advice - ɢᴇᴛ ʀᴀɴᴅᴏᴍ ᴀᴅᴠɪᴄᴇ ʙʏ ʙᴏᴛ
-/ai [ǫᴜᴇʀʏ] - ᴀsᴋ ʏᴏᴜʀ ǫᴜᴇsᴛɪᴏɴ ᴡɪᴛʜ ᴄʜᴀᴛɢᴘᴛ's ᴀɪ
-/gemini [ǫᴜᴇʀʏ] - ᴀsᴋ ʏᴏᴜʀ ǫᴜᴇsᴛɪᴏɴ ᴡɪᴛʜ ɢᴏᴏɢʟᴇ's ɢᴇᴍɪɴɪ ᴀɪ
-/bard [ǫᴜᴇʀʏ] -ᴀsᴋ ʏᴏᴜʀ ǫᴜᴇsᴛɪᴏɴ ᴡɪᴛʜ ɢᴏᴏɢʟᴇ's ʙᴀʀᴅ ᴀɪ"""
+/advice - Get random advice from the bot
+/ai [query] - Ask your question using ChatGPT's AI
+/gemini [query] - Ask your question using Google's Gemini AI
+/bard [query] - Ask your question using Google's Bard AI
+"""
