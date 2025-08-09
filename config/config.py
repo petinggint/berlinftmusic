a39fba82fae1896bf5d761b72c108e00d3b6d3f8
petinggint <155607304+petinggint@users.noreply.github.com>
2025-08-09 13:25:07 +0700

    b config.py

--- config/config.py
+++ config/config.py
@@ -86,7 +86,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Vivekkumar-IN/YukkiMusic",
+    "https://github.com/petinggint/berlinftmusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
